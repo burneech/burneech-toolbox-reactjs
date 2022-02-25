811a6fada79c485ba7e11c05ab5b05fc5a3ff687
Mateo Brnic <mateo.brnic@gmail.com>
2022-02-25 20:29:42 +0100

    Added support for MUI themes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react"
+import "@fontsource/roboto"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import {
+  Switch,
+  Container,
+  Typography,
+  FormGroup,
+  FormControlLabel,
+  CssBaseline,
+} from "@mui/material"
+import AccordionComponent from "./AccordionComponent/"
+
+const dark = { palette: { mode: "dark" } }
+const light = { palette: { mode: "light" } }
+
 export default function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(true)
+  function changeTheme() { setIsDarkTheme(!isDarkTheme) }
+
   return (
-    <div>
-      <h1>Hello from app!</h1>
-    </div>
+    <ThemeProvider theme={isDarkTheme ? createTheme(dark) : createTheme(light)}>
+      <CssBaseline />
+      <Container>
+        <FormGroup>
+          <FormControlLabel
+            control={<Switch checked={isDarkTheme} onChange={changeTheme} />}
+            label={isDarkTheme ? "Dark" : "Light"} />
+        </FormGroup>
+
+        <Typography variant="h4" gutterBottom component="div">
+          The theme is {isDarkTheme ? "dark" : "light"}
+        </Typography>
+
+        <AccordionComponent />
+      </Container>
+    </ThemeProvider>
   )
 }
