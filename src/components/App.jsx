a352bd5f8f7eb3668462c033fd73ec29f8684bf0
burneech <81528115+burneech@users.noreply.github.com>
2022-02-26 23:39:01 +0100

    Added LinkedIn and Github buttons, configured the favicon.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react"
 import "@fontsource/roboto"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
-import { Switch, Container, FormGroup, FormControlLabel, CssBaseline } from "@mui/material"
-import Data from "../data/ToolboxData.json"
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import {
+  Switch, Container, FormGroup, IconButton, Grid, Link,
+  FormControlLabel, CssBaseline, Typography, Stack, Divider
+} from "@mui/material"
+
 import Category from "./Category/"
+import Data from "../data/ToolboxData.json"
 import logo from '../data/logo.svg'
 import '../index.css'
 
@@ -23,17 +29,43 @@ export default function App() {
       <CssBaseline />
       <Container>
 
-        <FormGroup>
-          <FormControlLabel
-            control={<Switch checked={isDarkTheme} onChange={changeTheme} />}
-            label={isDarkTheme ? "Dark" : "Light"} />
-        </FormGroup>
+        <Grid container justifyContent="flex-end">
+          <FormGroup>
+            <FormControlLabel
+              control={<Switch checked={isDarkTheme} onChange={changeTheme} />}
+              label={isDarkTheme ? "Dark" : "Light"} />
+          </FormGroup>
+        </Grid>
 
         <img className="logo" src={logo} alt="logo" />
 
+        <Typography variant="h4" sx={{ textShadow: 10, textAlign: 'center' }}>
+          Welcome to my web-toolbox.
+        </Typography>
+
+        <Typography variant="subtitle2" sx={{ mb: 4, textAlign: 'center' }} color="text.secondary">
+          On this website you can find collections of useful (and mostly free) online tools and websites.
+        </Typography>
+
         {CategoriesList}
 
+        <Stack alignItems="center" sx={{ mt: 4, mb: 4 }}>
+          <Grid>
+            <IconButton aria-label="github" href="https://github.com/burneech" target="_blank">
+              <GitHubIcon />
+            </IconButton>
+            <IconButton aria-label="linkedin" href="https://linkedin.com/in/mateo-brnic" target="_blank">
+              <LinkedInIcon />
+            </IconButton>
+          </Grid>
+          <Link href="https://www.vecteezy.com/free-vector/toolbox" target="_blank" underline="none">
+            <Typography variant="caption" color="text.secondary">
+              Toolbox Vectors by Vecteezy
+            </Typography>
+          </Link>
+        </Stack>
+
       </Container>
-    </ThemeProvider>
+    </ThemeProvider >
   )
 }
